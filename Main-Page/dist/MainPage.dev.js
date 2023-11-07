af5e42f2ded97d5a27f5af46a1c6e9205e7b6d8b
Miles F <miles.fritzmather@gmail.com>
2023-11-06 21:51:21 -0600

    Working on animation functionality. Half way through making hover effect gradient on text

--- Main-Page/dist/MainPage.dev.js
+++ Main-Page/dist/MainPage.dev.js
@@ -1,1 +1,37 @@
 "use strict";
+
+var realityText = document.getElementById("reality");
+realityText.addEventListener("mouseover", function () {
+  realityText.animate([{
+    color: 'red'
+  }], {
+    duration: 2000,
+    easing: 'ease-in',
+    delay: 0,
+    iterations: 1,
+    direction: 'alternate',
+    fill: 'both'
+  });
+});
+realityText.addEventListener("mouseout", function () {
+  console.log("test");
+});
+
+function zoomin(e) {
+  e.style.zIndex = "1000";
+  console.log(e.style.zIndex);
+
+  e.parentNode.animate([{
+    transform: 'scale(20)',
+    color: 'red'
+  }], {
+    duration: 500,
+    easing: 'ease-in',
+    delay: 0,
+    iterations: 1,
+    direction: 'alternate',
+    fill: 'both'
+  }).onfinish = function () {
+    window.location.href = "../Application-Page/ApplicationMain.html";
+  };
+}
