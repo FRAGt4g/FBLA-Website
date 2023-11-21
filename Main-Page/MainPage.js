6c4e728ac82e23ed1f288abd6d1a004c43cae33d
FRAGt4g <90790095+FRAGt4g@users.noreply.github.com>
2023-11-21 17:28:46 +0000

    Adding nicer accordion elements to main page

--- Main-Page/MainPage.js
+++ Main-Page/MainPage.js
@@ -1,24 +1,28 @@
 const realityText = document.getElementById("reality");
+const accordion = document.getElementsByClassName("accordionElement");
 
-realityText.addEventListener("mouseover", function() {
-    realityText.animate(
-        [
-            { 
-                color: 'red',
-            }
+for (i = 0; i < accordion.length; i++) {
+    console.log(accordion[i].innerHTML);
+    console.log("child: " + accordion[i].children[0].innerHTML);
+    accordion[i].children[0].addEventListener('click', function() {
+        this.parentNode.classList.toggle('active');
+
+        this.children[1].animate([
+            { transform: 'rotate(0deg)' },
+            { innerHTML: '-' },
+            { transform: 'rotate(180deg)' },
         ], {
-            duration: 2000,
-            easing: 'ease-in',
+            duration: 500,
+            easing: 'ease-in-out',
             delay: 0,
             iterations: 1,
-            direction: 'alternate',
-            fill: 'both'
-        },
-    )
-});
-realityText.addEventListener("mouseout", function() {
-    console.log("test");
-});
+            direction: 'normal',
+            fill: 'forwards'
+        });
+
+        this.children[1].innerHTML = (this.children[1].innerHTML === '+') ? '-' : '+';
+    })
+}
 
 function zoomin(e) {
     e.style.zIndex = "1000";
