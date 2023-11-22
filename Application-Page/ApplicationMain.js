9a51fb31de6b20045d96dc03279a85462b3007eb
Matthew Shi <matthewtershi@gmail.com>
2023-11-21 20:49:23 -0600

    i hate bootstrap

---
+++ Application-Page/ApplicationMain.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('btn');
+console.log("Hello World");
+
+/*form.addEventListener('submit', function(e) {
+    alert("TEMP");
+    e.preventDefault();
+
+    const fd = new FormData(form);
+
+})*/
